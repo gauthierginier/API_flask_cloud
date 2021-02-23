120171c2f6113157634b3be547035a6015bca208
DamienPlantin <damien2809@gmail.com>
2021-02-23 15:32:08 +0100

    Logging OK

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 def readcsv(*colonnes):
+    logging.debug(f"Utilisation de la fonction readcsv({colonnes})")
+    logging.info("Lecture du fichier csv")
     return pandas.read_csv(
         'data.csv',
         header=2,
@@ -17,31 +19,37 @@ def readcsv(*colonnes):
             'Year',
             'Emission',
             'Value',
-            'footnote',
-            'source'],
+            'Footnote',
+            'Source'],
         usecols=colonnes)
 
 
 @app.route('/')
 def hello_world():
+    logging.debug("Utilisation de la fonction hello_world()")
+    logging.debug("L'app fonctionne correctement")
     # utilisé pour tester si l'app fonctionne bien
     return 'Hello, World!'
 
 
 def allcountries():
+    logging.debug("Utilisation de la fonction allcountries()")
     df = readcsv('Region')
-    countries = list(set(df['Region'].to_list()))
+    countries = list(sorted(set(df['Region'].to_list())))
+    logging.debug(countries)
     return countries
 
 
 def bycountry(country):
+    logging.debug(f"Utilisation de la fonction bycountry({country.title()})")
     df = readcsv('Region', 'Year', 'Value')
     df = df.loc[df['Region'].isin([country.title()])]
     df = df.sort_values(by='Year', ascending=False)
     res = {}
-    res["country"] = str(df.iloc[0][0])
-    res["year"] = int(df.iloc[0][1])
-    res["emissions"] = float(df.iloc[0][2])
+    res["Country"] = str(df.iloc[0][0])
+    res["Year"] = int(df.iloc[0][1])
+    res["Emissions"] = float(df.iloc[0][2])
+    logging.debug(res)
     return json.dumps(res)
 
 
@@ -49,29 +57,35 @@ def bycountry(country):
 def by_country(country):
     # on veut la valeur la plus récente
     # des emissions totales pour le pays demandé
-    logging.debug(f"Pays demandé : {country}")
+    logging.debug(f"Utilisation de la fonction by_country({country.title()})")
     if country.title() in allcountries():
+        logging.debug(f"Pays demandé : {country.title()}")
         return bycountry(country)
     else:
+        logging.warning(f"Le pays {country.title()} n'est pas dans la liste")
         return json.dumps({
             "message": "Le pays choisi n'est pas dans la liste"})
         abort(404)
 
 
 def allyears():
+    logging.debug("Utilisation de la fonction allyears()")
     df = readcsv('Year')
-    years = list(set(df['Year'].to_list()))
+    years = list(sorted(set(df['Year'].to_list())))
+    logging.debug(years)
     return years
 
 
 def byyear(year):
+    logging.debug(f"Utilisation de la fonction byyear({year})")
     globalemission = ["Emissions (thousand metric tons of carbon dioxide)"]
     df = readcsv('Year', 'Value', 'Emission')
     df = df.loc[df['Year'].isin([str(year)])]
     df = df.loc[df['Emission'].isin(globalemission)]
     res = {}
     res["Year"] = year
-    res["Total"] = df['Value'].mean()
+    res["Total"] = round(df['Value'].mean(), 3)
+    logging.debug(res)
     return json.dumps(res)
 
 
@@ -79,17 +93,19 @@ def byyear(year):
 def average_for_year(year):
     # on cherche la moyenne des émissions
     # totales au niveau mondial pour une année demandée
+    logging.debug(f"Utilisation de la fonction average_for_year({year})")
     logging.debug(f"Année demandée : {year}")
     if int(year) in allyears():
         return byyear(year)
-        # return jsonify({"year":"1975", "total":12333555.9})
     else:
+        logging.warning(f"L'année {year} n'est pas dans la liste")
         return json.dumps({
             "message": "L'année choisie n'est pas dans la liste"})
         abort(404)
 
 
 def bypercapita(country):
+    logging.debug(f"Utilisation de la fonction bypercapita({country.title()})")
     capita = ["Emissions per capita (metric tons of carbon dioxide)"]
     df = readcsv('Region', 'Year', 'Emission', 'Value')
     df = df.loc[df['Region'].isin([country.title()])]
@@ -100,19 +116,25 @@ def bypercapita(country):
     while i < nbannee:
         res[int(df.iloc[i][1])] = float(df.iloc[i][3])
         i += 1
+    logging.debug(res)
     return json.dumps(res)
 
 
 @app.route('/per_capita/<country>')
 def per_capita(country):
-    logging.debug(f"Pays demandé : {country}")
+    logging.debug(f"Utilisation de la fonction per_capita({country.title()})")
     if country.title() in allcountries():
+        logging.debug(f"Pays demandé : {country.title()}")
         return bypercapita(country)
     else:
+        logging.warning(f"Le pays {country.title()} n'est pas dans la liste")
         return json.dumps({
             "message": "Le pays choisi n'est pas dans la liste"})
         abort(404)
 
 
 if __name__ == "__main__":
+    app.config.update(
+        ENV="development"
+    )
     app.run(debug=True)
