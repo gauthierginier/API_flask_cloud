6a54f93f7a8f194b7eecf26a587c026005bcefba
gauthierginier <gauthier.ginier@gmail.com>
2021-02-25 11:19:56 +0100

    separation des test requetes API en classe, requete par requete

--- testapi.py
+++ testapi.py
@@ -2,7 +2,7 @@ import unittest
 import app
 
 
-class TestApi(unittest.TestCase):
+class TestByCountry(unittest.TestCase):
     # check response status is 200
     def test_latest_by_country_status(self):
         tester = app.app.test_client(self)
@@ -22,6 +22,8 @@ class TestApi(unittest.TestCase):
         response = tester.get("/latest_by_country/Bulgaria")
         self.assertTrue(b'Year' in response.data)
 
+
+class TestAverageByYear(unittest.TestCase):
     # check response status is 200
     def test_average_by_year_status(self):
         tester = app.app.test_client(self)
@@ -40,6 +42,27 @@ class TestApi(unittest.TestCase):
         tester = app.app.test_client(self)
         response = tester.get("/average_by_year/2017")
         self.assertTrue(b'Total' in response.data)
+
+
+class TestPerCapita(unittest.TestCase):
+    # check response status is 200
+    def test_per_capita_status(self):
+        tester = app.app.test_client(self)
+        response = tester.get("/per_capita/Cameroon")
+        status_code = response.status_code
+        self.assertEqual(status_code, 200)
+
+    # check the response content
+    def test_per_capita_type(self):
+        tester = app.app.test_client(self)
+        response = tester.get("/per_capita/Cameroon")
+        self.assertEqual(response.content_type, "application/json")
+
+    # check the response DATA
+    def test_per_capita_data(self):
+        tester = app.app.test_client(self)
+        response = tester.get("/per_capita/Cameroon")
+        self.assertIsInstance(response.data[0], int)
 
 
 if __name__ == '__main__':
