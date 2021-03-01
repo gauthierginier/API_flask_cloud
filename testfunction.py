2bd569d9aca31a1626e8b5dd32d6a5ba86e465a1
DamienPlantin <damien2809@gmail.com>
2021-03-01 11:35:08 +0100

    modif testfunction suite lower du csv

--- testfunction.py
+++ testfunction.py
@@ -9,9 +9,9 @@ from readcsv import readcsv
 class TestApp(unittest.TestCase):
     def test_readcsv(self):
         # on test si la sortie de la fonction test_readcsv est de type class
-        self.assertIsInstance(type(readcsv()), type)
+        self.assertIsInstance(type(readcsv('Region')), type)
         # on test que la sortie est != de None
-        self.assertIsNotNone(readcsv())
+        self.assertIsNotNone(readcsv('Region'))
 
     def test_allcountry(self):
         # test que la fonction retourne un liste
@@ -23,12 +23,12 @@ class TestApp(unittest.TestCase):
     def test_bycountry(self):
         # test que la fonction retourne un dictionnaire
         # test de 2 exemples
-        self.assertIs(type(func_annexe.bycountry("Cameroon")), dict)
+        self.assertIs(type(func_annexe.bycountry("cameroon")), dict)
         self.assertEqual(
-            func_annexe.bycountry("Cameroon"),
+            func_annexe.bycountry("cameroon"),
             {'Country': 'Cameroon', 'Year': 2017, 'Emissions': 6152.919})
         self.assertEqual(
-            func_annexe.bycountry("Serbia"),
+            func_annexe.bycountry("serbia"),
             {'Country': 'Serbia', 'Year': 2017, 'Emissions': 46129.569})
 
     def test_allyears(self):
@@ -52,13 +52,13 @@ class TestApp(unittest.TestCase):
     def test_bypercapita(self):
         # test que la fonction retourne un dictionnaire
         # test de 2 exemples
-        self.assertIs(type(func_annexe.bypercapita('Cameroon')), dict)
+        self.assertIs(type(func_annexe.bypercapita('cameroon')), dict)
         self.assertEqual(
-            func_annexe.bypercapita('Cameroon'),
+            func_annexe.bypercapita('cameroon'),
             {1975: 0.137, 1985: 0.237, 1995: 0.183, 2005: 0.169,
              2010: 0.253, 2015: 0.26, 2016: 0.26, 2017: 0.256})
         self.assertEqual(
-            func_annexe.bypercapita('Yemen'),
+            func_annexe.bypercapita('yemen'),
             {1975: 0.257, 1985: 0.493, 1995: 0.616, 2005: 0.915,
              2010: 0.948, 2015: 0.433, 2016: 0.34, 2017: 0.316})
 
